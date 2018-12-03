1f0a9450295306c7720af7676b7d84c8578514c0
Joojihwan <31200862+Joojihwan@users.noreply.github.com>
2018-12-03 18:49:57 +0900

    TedPermission
    라이브러리 추가

--- Moon/app/src/main/java/com/bist/moon/MainActivity.java
+++ Moon/app/src/main/java/com/bist/moon/MainActivity.java
@@ -1,7 +1,13 @@
 package com.bist.moon;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*
+    TedPermission.with(this)
+            .setPermissionListener(permissionlistener)
+            .setRationaleMessage("구글 로그인을 하기 위해서는 주소록 접근 권한이 필요해요")
+    .setDeniedMessage("왜 거부하셨어요...\n하지만 [설정] > [권한] 에서 권한을 허용할 수 있어요.")
+    .setPermissions(Manifest.permission.READ_CONTACTS)
+    .check();
+    */
+    PermissionListener permissionlistener;
+
+    {
+        permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                Toast.makeText(MainActivity.this, "권한 허가", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                Toast.makeText(MainActivity.this, "권한 거부\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+            }
+        };
     }
 }
