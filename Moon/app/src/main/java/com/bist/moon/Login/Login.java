d511474af8a7c6b182e0394556ba7cfe3ec82c33
Joojihwan <31200862+Joojihwan@users.noreply.github.com>
2018-12-04 16:54:58 +0900

    로그인끄적

--- Moon/app/src/main/java/com/bist/moon/Login/Login.java
+++ Moon/app/src/main/java/com/bist/moon/Login/Login.java
@@ -2,14 +2,39 @@ package com.bist.moon.Login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.bist.moon.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
+    
+    FirebaseUser user;
+    FirebaseAuth firebaseAuth;
+
+    EditText editID, editPW;
+    Button btnLogin, btnReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editID = findViewById(R.id.editID);
+        editPW = findViewById(R.id.editPW);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnReg = findViewById(R.id.btnReg);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+
 }
--- Moon/app/src/main/java/com/bist/moon/MainActivity.java
+++ Moon/app/src/main/java/com/bist/moon/MainActivity.java
@@ -1,20 +1,61 @@
 package com.bist.moon;
 
 import android.Manifest;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bist.moon.Login.Login;
 import com.gun0912.tedpermission.PermissionListener;
 
+import java.sql.Time;
 import java.util.ArrayList;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    long now = 0;
+    int cnt = 0;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.testview);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(now == 0) {
+                    now = System.currentTimeMillis();
+                }
+                else if(System.currentTimeMillis()- now <= 500){
+                    now = System.currentTimeMillis();
+                    cnt++;
+                }
+                else{
+                    now = 0;
+                    cnt = 0;
+                }
+
+                switch (cnt){
+                    case 3 : Toast.makeText(MainActivity.this,"3!",Toast.LENGTH_SHORT).show();
+                    break;
+                    case 4 : Toast.makeText(MainActivity.this,"2!",Toast.LENGTH_SHORT).show();
+                    break;
+                    case 5 : Toast.makeText(MainActivity.this,"1!",Toast.LENGTH_SHORT).show();
+                    break;
+                    case 6 :
+                        Intent i = new Intent(MainActivity.this,Login.class);
+                        startActivity(i);
+                }
+            }
+        });
+
     }
 
     /*
@@ -31,12 +72,12 @@ public class MainActivity extends AppCompatActivity {
         permissionlistener = new PermissionListener() {
             @Override
             public void onPermissionGranted() {
-                Toast.makeText(MainActivity.this, "권한 허가", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "권한 허가", LENGTH_SHORT).show();
             }
 
             @Override
             public void onPermissionDenied(ArrayList<String> deniedPermissions) {
-                Toast.makeText(MainActivity.this, "권한 거부\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "권한 거부\n" + deniedPermissions.toString(), LENGTH_SHORT).show();
             }
         };
     }
